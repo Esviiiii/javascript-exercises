b2a545c3b6c76e3ebf70372f18ac7896d1b429c4
Esviiiii <shawnlas0816@gmail.com>
2026-07-31 23:01:43 +0800

    working problem 8

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, itemToRemove) {
+    return arr.filter(item => item !== itemToRemove);
 };
+
+removeFromArray([1, 2, 3, 4], 2);
 
 // Do not edit below this line
 module.exports = removeFromArray;
