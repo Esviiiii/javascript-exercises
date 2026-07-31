c668c4df22a41d4f906bccdfbc65efd3a4d0dfb5
Esviiiii <shawnlas0816@gmail.com>
2026-07-31 22:41:25 +0800

    working task 7

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+    let result = "";
+
+    for(let i = 0; i <= string.length; i++){
+        result += string.charAt(string.length - i);
+    }
+
+    return result;
 };
+
+reverseString("");
 
 // Do not edit below this line
 module.exports = reverseString;
