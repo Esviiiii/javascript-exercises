3ac9ef1e300436afadefab38bcca7ad5b88957ec
Esviiiii <shawnlas0816@gmail.com>
2026-07-31 22:06:37 +0800

    practice

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        module.log("Error")
+    } else  {
+        for(let i = 0; i === num; i++){
+            module.log(string)
+        }
+    }
 };
+
+console.log(repeatString("hey", 3))
 
 // Do not edit below this line
 module.exports = repeatString;
