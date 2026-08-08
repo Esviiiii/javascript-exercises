2f0bb9882ccb79275e78e194e8a3bbf6e5d1267f
Esviiiii <shawnlas0816@gmail.com>
2026-08-08 21:13:11 +0800

    pass first test

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,26 +1,54 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((total, nextValue) => total + nextValue , 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, nextValue) => total * nextValue , 1)
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+  return x
+}
+
+const factorial = function(num) {
+  let total = 0;
+  
+  if(num === 0){
+    return 1;
+  } else {
+    for(let i = num; num > 0; i--){
+      total += num * (i - 1);
+    }
+    return total;
+  }
 };
 
-const factorial = function() {
-	
-};
+add(0, 0)
+add(2, 2)
+add(2, 6)
+
+subtract(10, 4)
+subtract(-10, -4)
+subtract(-8, 7)
+
+sum([])
+sum([7])
+sum([7, 11])
+sum([1, 3, 5, 7, 9])
+
+multiply([2, 4])
+multiply([2, 4, 6, 8, 10, 12, 14])
+
+factorial(0)
+factorial(2)
 
 // Do not edit below this line
 module.exports = {
--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let text = "";
 
+    for(let i = 0; i <= string.length; i++){
+        text += string.charAt(string.length - i)
+    };
+
+    if (string === text) {
+        return true;
+    } else {
+        return false;
+    }
 };
+
+palindromes("racecar")
 
 // Do not edit below this line
 module.exports = palindromes;
